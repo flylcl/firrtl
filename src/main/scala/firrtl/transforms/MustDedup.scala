f25caafa224d1f05a2d4b88b037ed7deb0dccd09
Jack Koenig <koenig@sifive.com>
2021-02-03 19:17:30 -0800

    Add pretty visualization of Deduplication failures

--- src/main/scala/firrtl/transforms/MustDedup.scala
+++ src/main/scala/firrtl/transforms/MustDedup.scala
@@ -9,6 +9,7 @@ import firrtl.analyses.InstanceKeyGraph
 import firrtl.analyses.InstanceKeyGraph.InstanceKey
 import firrtl.options.Dependency
 import firrtl.stage.Forms
+import firrtl.graph.DiGraph
 
 import java.io.{File, FileWriter}
 
@@ -30,13 +31,18 @@ case class MustDeduplicateReportDirectory(directory: String) extends NoTargetAnn
 object MustDeduplicateTransform {
   sealed trait DedupFailureCandidate {
     def message: String
+    def modules: Seq[OfModule]
   }
   case class LikelyShouldMatch(a: OfModule, b: OfModule) extends DedupFailureCandidate {
     def message: String = s"Modules '${a.value}' and '${b.value}' likely should dedup but do not!"
+    def modules = Seq(a, b)
   }
   case class DisjointChildren(a: OfModule, b: OfModule) extends DedupFailureCandidate {
     def message: String = s"Modules '${a.value}' and '${b.value}' have disjoint child instances!"
+    def modules = Seq(a, b)
   }
+
+  final class DeduplicationFailureException(msg: String) extends FirrtlUserException(msg)
 
   /** Reports deduplication failures two Modules
     *
@@ -66,7 +72,64 @@ object MustDeduplicateTransform {
     (allMismatches, candidates)
   }
 
+  // TODO move these to object or class DiGraph
+  private def prettifyDiGraph(graph: DiGraph[String]): String = prettifyDiGraph[String](graph)(x => x)
+  private def prettifyDiGraph[A](graph: DiGraph[A])(f: A => String): String = {
+    val (l, n, c) = {
+      val cs = options.DependencyManagerUtils.PrettyCharSet
+      (cs.lastNode, cs.notLastNode, cs.continuation)
+    }
+    val ctab = " " * c.size + " "
+    def rec(tab: String, node: A, mark: String, prev: List[String]): List[String] = {
+      val here = s"$mark${f(node)}"
+      val children = graph.getEdges(node)
+      val last = children.size - 1
+      children.toList // From LinkedHashSet to List to avoid determinism issues
+              .zipWithIndex // Find last
+              .foldLeft(here :: prev) {
+          case (acc, (nodex, idx)) =>
+            val nextTab = if (idx == last) tab + ctab else tab + c + " "
+            val nextMark = if (idx == last) tab + l else tab + n
+            rec(nextTab, nodex, nextMark + " ", acc)
+       }
+    }
+    graph.findSources
+         .foldLeft(Nil: List[String]) {
+      case (acc, root) => rec("", root, "", acc)
+    }.reverse.mkString("\n")
+  }
+
+  // Creates new DiGraph of only the nodes reachable from the passed arguments
+  private def subgraphReachableFrom[A](graph: DiGraph[A], roots: Set[A]): DiGraph[A] = {
+    val vprime = roots.flatMap(graph.reachableFrom(_)) ++ roots
+    graph.subgraph(vprime)
+  }
+
   private case class DedupFailure(shouldDedup: Seq[String], relevantMods: Set[OfModule], candidates: Seq[DedupFailureCandidate])
+
+  /** Turn a [[DedupFailure]] into a pretty graph for visualization */
+  private def makePrettyTreeOfFailure(failure: DedupFailure, graph: DiGraph[String], getParents: String => Seq[String]): String = {
+    val DedupFailure(shouldDedup, _, candidates) = failure
+    val shouldDedupSet = shouldDedup.toSet
+    // Create a graph rooted at the "shouldDedup" nodes
+    val mygraph =
+      subgraphReachableFrom(graph, shouldDedupSet) +
+      // Add fake nodes to represent parents of the "shouldDedup" nodes
+      DiGraph(shouldDedup.map(n => getParents(n).mkString(", ") -> n): _*)
+    // Gather candidate modules and assign indices for reference
+    val candidateIdx: Map[String, Int] =
+      candidates.zipWithIndex
+                .flatMap { case (c, idx) => c.modules.map(_.value -> idx) }
+                .toMap
+    // Now mark the graph for modules of interest
+    val markedGraph = mygraph.transformNodes { n =>
+      val next = if (shouldDedupSet(n)) s"($n)" else n
+      candidateIdx.get(n)
+                  .map(i => s"$next [$i]")
+                  .getOrElse(next)
+    }
+    prettifyDiGraph(markedGraph)
+  }
 }
 
 // Must run after ExpandWhens
@@ -98,12 +161,20 @@ class MustDeduplicateTransform extends Transform with DependencyAPIMigration {
           DedupFailure(moduleNames, relevantMods, candidates.distinct)
       }
     if (dedupFailures.nonEmpty) {
+      val modgraph = igraph.graph.transformNodes(_.module)
+      // Lookup the parent Module name of any Module
+      val getParents: String => Seq[String] =
+        modgraph.reverse
+                .getEdgeMap
+                .mapValues(_.toSeq)
       // Create and log reports
-      val reports = dedupFailures.map { case DedupFailure(shouldDedup, _, candidates) =>
+      val reports = dedupFailures.map { case fail @ DedupFailure(shouldDedup, _, candidates) =>
+        val graph = makePrettyTreeOfFailure(fail, modgraph, getParents)
         val msg =
           s"""${shouldDedup.mkString(", ")} are marked as "must deduplicate", but did not deduplicate.
+             |$graph
              |Failure candidates:
-             |${candidates.map(" - " + _.message).mkString("\n")}
+             |${candidates.zipWithIndex.map { case (c, i) => s" - [$i] " + c.message }.mkString("\n")}
              |""".stripMargin
         logger.error(msg)
         msg
@@ -133,6 +204,9 @@ class MustDeduplicateTransform extends Transform with DependencyAPIMigration {
         fw.write(mod.serialize)
         fw.close()
       }
+
+      val msg = s"Modules marked 'must deduplicate' failed to deduplicate! See error reports in $dirName"
+      throw new DeduplicationFailureException(msg)
     }
     state
   }
